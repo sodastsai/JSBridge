d08169ae25b4c01e1e4a9c9eac2cc9372cd8aeb5
sodastsai <sodas2002@gmail.com>
2016-01-28 14:27:43 +0800

    Add timer draft

---
+++ JSBridge/TCJSTimer.m
@@ -0,0 +1,18 @@
+//
+//  TCJSTimer.m
+//  JSBridge
+//
+//  Created by sodas on 1/28/16.
+//  Copyright © 2016 JSBridge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TCJSBridge/TCJSJavaScriptContext.h>
+
+@interface TCJSTimer : NSObject <TCJSJavaScriptContextExtension>
+
+@end
+
+@implementation TCJSTimer
+
+@end
